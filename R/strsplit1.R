aaf09f295fde7aaca159276cbeffc391b25c5dd4
Ian Cay <icay@calpoly.edu>
2026-05-20 09:08:28 -0700

    Adding export to NAMESPACE

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -7,8 +7,9 @@
 #' @export
 #'
 #' @examples
-#' x <- "alfa,bravo,charlie,delta"
+#' (x <- "alfa,bravo,charlie,delta")
 #' strsplit1(x, split = ",")
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
+load
